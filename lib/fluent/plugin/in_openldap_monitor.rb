fc3fa75b6a6766dc64bb825b8c471843c1302575
studio3104 <studio3104.com@gmail.com>
2014-05-16 19:46:13 +0900

    organize

--- lib/fluent/plugin/in_openldap_monitor.rb
+++ lib/fluent/plugin/in_openldap_monitor.rb
@@ -15,19 +15,21 @@ class Fluent::OpenLDAPMonitorInput < Fluent::Input
 
   def configure(conf)
     super
-    @auth = {
+
+    auth = {
       method: :simple,
       username: @bind_dn,
       password: @bind_password,
     }
+    @ldap = Net::LDAP.new(host: @host, port: @port, auth: auth)
   end
 
   def start
     super
-    @watcher = TimerWatcher.new(@check_interval, true, &method(:ldapkarastatusmonitornoataiwotottekru))
+    @watcher = TimerWatcher.new(@check_interval, true, &method(:ldapsearch))
     @loop = Coolio::Loop.new
     @watcher.attach(@loop)
-    @thread = Thread.new(&method(:run))
+    @thread = Thread.new(@loop.run)
   end
 
   def shutdown
@@ -36,39 +38,56 @@ class Fluent::OpenLDAPMonitorInput < Fluent::Input
     @thread.join
   end
 
-  def run
-    @loop.run
+  def ldapsearch
+    entries = @ldap.search(attributes: ['+'], base: 'cn=Monitor')
+    result = convert_entries(entries.flatten)
+    Fluent::Engine.emit(@tag, Fluent::Engine.now, result)
+  rescue => e
+    log.warn("#{e.message} (#{e.class.to_s}) #{e.backtrace.to_s}")
   end
 
   SUPPRESS_ATTRIBUTES = %w{ dn structuralobjectclass creatorsname modifiersname createtimestamp modifytimestamp monitortimestamp entrydn subschemasubentry hassubordinates }
-  def ldapkarastatusmonitornoataiwotottekru
+  def convert_entries(entries)
     result = {}
-    Net::LDAP.open(host: @host, port: @port, auth: @auth) do |ldap|
-      ldap.search(attributes: ['+'], base: 'cn=Monitor') do |entry|
-        dn = entry[:dn].first
-        dn = dn.split(',').map { |d| d.sub(/cn\=/,'').gsub(/\s+/,'_').downcase.to_sym }.reverse
+    entries.each do |entry|
+      values_of = {}
+      entry.each do |attribute, values|
+        next if SUPPRESS_ATTRIBUTES.include?(attribute.to_s)
+        values_of[attribute] = convert_values_type(values)
+      end
 
-        values_of = {}
-        entry.each do |attribute, values|
-          next if SUPPRESS_ATTRIBUTES.include?(attribute.to_s)
-          values_of[attribute] = values
-        end
+      dn = entry[:dn].first # e.g.) cn=Current,cn=Connections,cn=Monitor
+      dn = dn.split(',').map { |d| d.sub(/cn\=/,'').gsub(/\s+/,'_').downcase.to_sym }.reverse # e.g.) [:monitor, :connections, :current]
 
-        case dn.size
-        when 1
-          result[dn[0]] ||= values_of
-        when 2
-          result[dn[0]][dn[1]] ||= values_of
-        when 3
-          result[dn[0]][dn[1]][dn[2]] ||= values_of
-        when 4
-          result[dn[0]][dn[1]][dn[2]][dn[3]] ||= values_of
-        else raise dn.size.to_s
-        end
+      case dn.size
+      when 1
+        result[dn[0]] ||= values_of
+      when 2
+        result[dn[0]][dn[1]] ||= values_of
+      when 3
+        result[dn[0]][dn[1]][dn[2]] ||= values_of
+      when 4
+        result[dn[0]][dn[1]][dn[2]][dn[3]] ||= values_of
+      else raise dn.size.to_s
       end
     end
+    result
+  end
 
-    Fluent::Engine.emit(@tag, Fluent::Engine.now, result)
+  def convert_values_type(values)
+    values = [values].flatten.map { |v|
+      if v == 'TRUE'
+        true
+      elsif v == 'FALSE'
+        false
+      elsif v.match(/^\d+$/)
+        v.to_i
+      else
+        v
+      end
+    }
+
+    values.size == 1 ? values.first : values
   end
 
   class TimerWatcher < Coolio::TimerWatcher
